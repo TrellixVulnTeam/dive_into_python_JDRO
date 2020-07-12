a23ae386cf528e193e8300fefd02850baebb7125
15Andrew43 <avborovets@gmail.com>
2020-07-12 20:30:00 +0300

    modules and packages

---
+++ playground/mymodule.py
@@ -0,0 +1,8 @@
+# import mypackage.utils
+# from mypackage.utils import multiply
+from mypackage.utils import *
+
+
+if __name__ == "__main__": 
+	# print(mypackage.utils.multiply(2, 3))
+	print(multiply(2, 3))
