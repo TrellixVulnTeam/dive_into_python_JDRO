2f314c93304f24971c76a1c2ef6cb835e432896f
15Andrew43 <avborovets@gmail.com>
2020-07-15 12:53:56 +0300

    decorator is done

---
+++ week2/decorator.py
@@ -0,0 +1,19 @@
+import json
+import functools
+
+
+def to_json(func):
+	@functools.wraps(func)
+	def wrapped(*args, **kwargs):
+		result = func(*args, **kwargs)
+		return json.dumps(result)
+	return wrapped
+
+# @to_json
+# def get_data():
+#   return {
+#     'data': 42
+#   }
+  
+# print(type(get_data()))  # вернёт '{"data": 42}'
+# print(get_data.__name__)
